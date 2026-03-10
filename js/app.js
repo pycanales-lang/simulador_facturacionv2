ff40e5facc29764cb95d05496f0b6e5f11b78638
pycanales-lang <pycanales400@gmail.com>
2026-03-10 11:26:39 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -44,7 +44,7 @@ function simular() {
     const diffMeses = (hoy.getFullYear() - fechaInstalacionGlobal.getFullYear()) * 12 + (hoy.getMonth() - fechaInstalacionGlobal.getMonth());
     esCuentaNueva = diffMeses <= 4;
 
-    actualizarMesesUI(true);
+    actualizarMesesUI(timelineDias === 90);
 
     const posInst = (diaInst / timelineDias) * 100;
         
@@ -369,6 +369,7 @@ function calcularProximoEvento(posActual){
 
     return `Próximo evento: ${proximo.nombre} en ${diasRestantes} días`;
 }
+
 
 
 
